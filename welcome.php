d6ba5d36f5561193f94ee28454e7b2a38de29164
n8greene27 <139074335+n8greene27@users.noreply.github.com>
2023-07-21 17:54:00 -0600

    Final website update

--- welcome.php
+++ welcome.php
@@ -3,7 +3,7 @@
 
 Welcome <?php echo $_POST["name"]; ?><br>
 Your email address is: <?php echo $_POST["email"]; ?><br>
-Comment: <?php echo $_POST["comment"]; ?>
+Comment: <?php echo $_POST["comment"]; ?> 
 
 </body>
-</html>
+</html> 
